cd3c8b3b4c53c36484e1bb2717d2e4d214265260
edgiemarielle <edgiemarielle@gmail.com>
2026-09-08 20:30:59 +0800

    bom js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,24 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list');
+
+
+buttonElement.addEventListener('click', function () {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = '';
+    }
+    input.focus();
+});
+
